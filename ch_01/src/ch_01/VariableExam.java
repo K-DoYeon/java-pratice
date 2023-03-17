203389454e9e800d24746c1ec3214183c2b7ea38
K-DoYeon <loveworud9@gmail.com>
2023-03-17 17:42:40 +0900

    ch01

---
+++ ch_01/src/ch_01/VariableExam.java
@@ -0,0 +1,29 @@
+package ch_01;
+
+public class VariableExam {
+
+	public static void main(String[] args) {
+		//변수 value를 선언
+		int value = 10;
+		int result = value + 20;
+		System.out.println(result);
+		
+		
+		/*
+		  리터럴의 종류 : 정수, 실수, 문자, 문자열, 논리
+		  
+		  정수 리터럴 : 소수점이 없는 정수, 10진수로 간주
+				   : 0으로 시작되는 리터럴은 8진수로 간주한다.( 02,03...)
+				   : 0x또는 0X로 시작하고 0~9 a~f 되어 있는 리터럴은 16진수로 간주(0x5, 0xA, 0xa38)
+		  		   :정수 리터럴을 저장할 수 있는 타입 byte, char,short,int,long과 같은 5가지 타입이다.
+		  실수 리터럴(float, double) : 소수점이 있는 리터럴은 10진수 실수로 간주한다. (0.25, -3.21)
+		  
+		  5E7 or 5e7 (지수와 가수)
+		  5E7 -> 5 *10 7승
+		  0.12e-5 -> 0.12*10의 -5승
+		  
+		  
+		 */
+	}
+
+}
